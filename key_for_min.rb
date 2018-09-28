3426a672406d15d8a44fc850521b9cfaa1dbd046
Solee Lee <carolinesoleelee@gmail.com>
2018-09-28 18:42:29 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,5 +4,20 @@
 def key_for_min_value(name_hash)
  if name_hash.length == 0
    return nil
+ else 
+   new = ""
+   smallest = nil
+   name_hash.collect do |x,value|
+     if(smallest == nil)
+       smallest = value
+       new = x 
+  else
+    if(smallest>value)
+      smallest = value
+      new = x
+    end
+  end
+end
+new
 end
 end
